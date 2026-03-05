868bb1c2114e7ff0891e58d4a2bcd2638a9f7318
minal440 <minalpshinde0@gmail.com>
2026-03-05 19:11:42 +0530

    Fixed evaluation plots saving

--- src/train_lstm.py
+++ src/train_lstm.py
@@ -19,6 +19,7 @@ data = pd.read_csv("D:/IoT_Project/datasets/iot_message_dataset.csv")
 texts = data["message"]
 labels = data["category"]
 
+
 encoder = LabelEncoder()
 y = encoder.fit_transform(labels)
 
@@ -32,6 +33,7 @@ X = pad_sequences(sequences, maxlen=10)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
 model = Sequential()
 
 model.add(Embedding(input_dim=2000, output_dim=128))
@@ -57,39 +59,47 @@ history = model.fit(
     batch_size=4,
     validation_data=(X_test, y_test)
 )
-predictions = model.predict(X_test)
 
+predictions = model.predict(X_test)
 pred_labels = predictions.argmax(axis=1)
 
 print("\nClassification Report:\n")
-
 print(classification_report(y_test, pred_labels))
+
+
 cm = confusion_matrix(y_test, pred_labels)
 
+plt.figure(figsize=(6,4))
 sns.heatmap(cm, annot=True, cmap="Blues")
 
 plt.title("Confusion Matrix")
-
 plt.xlabel("Predicted")
-
 plt.ylabel("Actual")
 
-plt.show()
-plt.plot(history.history["accuracy"], label="train")
-plt.plot(history.history["val_accuracy"], label="validation")
+plt.savefig("D:/IoT_Project/images/confusion_matrix.png")
+
+
+plt.close()
+
+plt.figure(figsize=(6,4))
+
+plt.plot(history.history["accuracy"], label="Train Accuracy")
+plt.plot(history.history["val_accuracy"], label="Validation Accuracy")
+
 plt.title("Model Accuracy")
 plt.xlabel("Epoch")
 plt.ylabel("Accuracy")
+
 plt.legend()
 
-plt.show()
+plt.savefig("D:/IoT_Project/images/accuracy_plot.png")
+
+plt.close()
+
+
 model.save("../models/lstm_classifier.h5")
 
-pickle.dump(tokenizer, open("../models/tokenizer.pkl", "wb"))
-
-pickle.dump(encoder, open("../models/encoder.pkl", "wb"))
+pickle.dump(tokenizer, open("D:/IoT_Project/models/tokenizer.pkl", "wb"))
+pickle.dump(encoder, open("D:/IoT_Project/models/encoder.pkl", "wb"))
 
 print("\nModel and tokenizer saved successfully.")
-
-plt.savefig("D:/IoT_Project/images/confusion_matrix.png")
-plt.savefig("D:/IoT_Project/images/accuracy_plot.png")
